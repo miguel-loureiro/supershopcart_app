d4fee3385ff15129d4a0b74e8b1449c8679ab9b9
miguel-loureiro <miguel.silvaloureiro@gmail.com>
2025-07-17 17:48:39 +0100

    iniital project structure

---
+++ src/main/java/com/supershopcart/SupershopcartApplication.java
@@ -0,0 +1,13 @@
+package com.supershopcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupershopcartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupershopcartApplication.class, args);
+	}
+
+}
